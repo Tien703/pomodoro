b816dd8f42e3aba1ac6bdada35b96381b3509cb4
Ti3n703 <ti3n703@gmail.com>
2025-10-13 06:31:16 +0700

    add stop and reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 
 function CountDown() {
   const [totalSeconds, setTotalSeconds] = useState(1500);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null)
 
   //handle countdown
-  const handleStart = () => {
-    setInterval(() =>{
+  CountDown(()=>{
+    if (isRunning) {
+
+     intervalRef.current = setInterval(() =>{
       setTotalSeconds(prevSeconds => prevSeconds - 1);
     },1000)
+    }
+  })
+  const handleStart = () => {
   };
+  const handlePause = () =>{
+    clearInterval(handleStart);
+  }
   //convert second to minute + second 
   const formatTime = (totalSeconds) => {
     const minutes = Math.floor(totalSeconds / 60)
@@ -24,6 +34,7 @@ function CountDown() {
     <div>
       <div>{formatTime(totalSeconds)}</div>
       <button onClick={handleStart}>start</button>
+      <button onClick={handlePause}>pause</button>
     </div>
   )
 
@@ -34,9 +45,7 @@ function App() {
       <CountDown/>
     </div>
   )
-
-  
-
 }
 
 export default App
+
