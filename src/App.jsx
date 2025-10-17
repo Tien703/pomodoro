871598d3abb144f323fa0cbb59c8d5e4eeb4bc8e
Ti3n703 <ti3n703@gmail.com>
2025-10-17 12:51:07 +0700

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,11 @@
-import { useState, useEffect, useRef } from 'react'
+import {Pomodoro} from './feature/Pomodoro/PomodoroPage'
 import './App.css'
 
 
-function CountDown() {
-  const [totalSeconds, setTotalSeconds] = useState(1500);
-  const [isRunning, setIsRunning] = useState(false);
-  const intervalRef = useRef(null)
-
-  //handle countdown
-  useEffect(()=>{
-    if (isRunning) {
-      intervalRef.current = setInterval(() => {
-        setTotalSeconds(prevSeconds => prevSeconds - 1);
-      },1000);
-    } 
-    else if (!isRunning && intervalRef.current !==null) {
-      clearInterval(intervalRef.current)
-    }
-    return () => {clearInterval(intervalRef)}
-  },[isRunning]);
-
-
-  const handleToogle = () =>{
-    setIsRunning(!isRunning)
-  };
-  const handleReset = () =>{
-    setIsRunning(false)
-    setTotalSeconds(1500)
-  };
-
-    //convert second to minute + second 
-  const formatTime = (totalSeconds) => {
-    const minutes = Math.floor(totalSeconds / 60)
-    const seconds = totalSeconds - minutes*60
-    const formatMinutes = String(minutes).padStart(2,'0') 
-    const formatSeconds = String(seconds).padStart(2,'0') 
-    return `${formatMinutes}:${formatSeconds}`;
-  }
-
-  return (
-    <div>
-      <div>{formatTime(totalSeconds)}</div>
-      <div>{!isRunning ? ( <button onClick={handleToogle}>start</button>) : (<button onClick={handleToogle}>pause</button>)}</div>
-      <button onClick={handleReset}>Reset</button>
-    </div>
-  )
-
-}
 function App() {
   return (
     <div>
-      <CountDown/>
+      <Pomodoro/>
     </div>
   )
 }
---
+++ src/feature/Pomodoro/PomodoroPage.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect, useRef } from 'react'
+
+export function Pomodoro() {
+  const [totalSeconds, setTotalSeconds] = useState(1500);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null)
+
+  //handle countdown
+  useEffect(()=>{
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setTotalSeconds(prevSeconds => prevSeconds - 1);
+      },1000);
+    } 
+    else if (!isRunning && intervalRef.current !==null) {
+      clearInterval(intervalRef.current)
+    }
+    return () => {clearInterval(intervalRef)}
+  },[isRunning]);
+
+
+  const handleToogle = () =>{
+    setIsRunning(!isRunning)
+  };
+  const handleReset = () =>{
+    setIsRunning(false)
+    setTotalSeconds(1500)
+  };
+
+    //convert second to minute + second 
+  const formatTime = (totalSeconds) => {
+    const minutes = Math.floor(totalSeconds / 60)
+    const seconds = totalSeconds - minutes*60
+    const formatMinutes = String(minutes).padStart(2,'0') 
+    const formatSeconds = String(seconds).padStart(2,'0') 
+    return `${formatMinutes}:${formatSeconds}`;
+  }
+
+  return (
+    <div>
+      <div>{formatTime(totalSeconds)}</div>
+      <div>{!isRunning ? ( <button onClick={handleToogle}>start</button>) : (<button onClick={handleToogle}>pause</button>)}</div>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  )
+
+}
